36b7a61506d7c495dba5a0b3d98decd08875a759
lamyaeakhtr <lamya.akh@hotmail.fr>
2022-03-20 23:11:59 +0100

    ajout de la page connecter

---
+++ connecter.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta http-equiv="Content-Type"
+content="text/html; charset=UTF-8" />
+
+<link rel="stylesheet" href="styles/styleindex.css" type="text/css" media="screen" />
+
+<title>Connecter</title>
+
+</head>
+<body>
+
+
+
+<?php 
+include('php/bdd.php');
+session_start();
+
+$bdd = getBD();
+
+
+$rep = $bdd->query('select * from utilisateur where pseudo="'.$_POST['pseudo'].'"');
+while ($ligne =$rep ->fetch()) 
+{
+ echo $ligne['nom']."<br/>";
+ echo $ligne['prenom'];
+ echo $ligne['pseudo'];
+
+ if($_POST['pseudo']=='' || $_POST['mdp']=='')
+{
+   echo '<meta http-equiv="refresh" content="0;url=connexion.php"/>'; 
+}
+else if( $ligne['pseudo']!=$_POST['pseudo'] || $ligne['mdp']!=$_POST['confmdp'] )
+{
+ echo 'Mauvais identifiant ou mauvais mot de passe';
+}
+else
+{
+    $_SESSION['utili']=array(
+       $ligne['pseudo'],
+       $ligne['nom'],
+       $ligne['prenom'],
+       $ligne['email'],
+       $ligne['mdp']);
+	   
+    $_SESSION['nom']= $ligne['nom'];
+    $_SESSION['prenom']= $ligne['prenom'];
+    $_SESSION['pseudo']=  $ligne['pseudo'];
+   echo '<meta http-equiv="refresh" content="0;url=index.php"/>';
+}
+
+
+}
+
+?>
+
+
+</body>
+</html>
