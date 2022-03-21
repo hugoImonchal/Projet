f9d7293ae4303dbeae37e47f35b1e07c04f80fa6
lamyaeakhtr <lamya.akh@hotmail.fr>
2022-03-21 15:29:39 +0100

    ajout de profil au bandeau

--- film.php
+++ film.php
@@ -1,15 +1,45 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-<link rel="stylesheet" href="../styles/stylearticles.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="styles/style.css" type="text/css" media="screen" />
 
 <meta http-equiv="Content-Type"
 content="text/html; charset=UTF-8" />
 <title>Film</title>
+<header class="main">	
+		<nav>
+			<ul class="menu">
+			<li>
+				<a href="index.php"> Where2Watch</a>
+				</li>
+				<li>
+				<a href="acceuil.php">Accueil</a>
+				</li>
+				<?php
+				if(isset($_SESSION['utili'])){
+					echo '<li> <a href="deconnexion.php">Deconnexion </a> </li>';
+					echo '<li> <a href"profil.php"> Profil </a> </li>';
+				}
+				else{
+				echo '<li> <a href="connexion.php"> Connexion </a> </li> ';
+				echo '<li> <a href="FormulaireInscription.php"> Inscription </a> </li>';} ?>
+				
+				<li>
+				<a href="contact/contact.php">Contact</a>
+				</li>
+				
+			</ul>
+		</nav>
+	</header>
 </head>
 
-<body>
 
+<body>
+<br>
+<br>
+<br>
+<br>
 <h1>Film</h1>
 
 <br />
@@ -35,7 +65,7 @@ content="text/html; charset=UTF-8" />
 					
 				?>
 
-<h2><a href="index.html" target="_blank"> Accueil </a></h2>
+<h2><a href="acceuil.php" > Retour </a></h2>
 
 </body>
 </html>
--- formulaire.php
+++ formulaire.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <?php
 include('php/bdd.php');
@@ -31,12 +32,36 @@ else{$ismdp=1; $mdp=$_POST['mdp'];}
 ?>
 <html lang="fr">
 <head>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<link rel="stylesheet" href="styles/style.css" type="text/css" media="screen" />
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Formulaire</title>
-  
+  <header class="main">	
+		<nav>
+			<ul class="menu">
+      <li>
+				<a href="index.php"> Where2Watch</a>
+				</li>
+				<li>
+				<a href="acceuil.php">Accueil</a>
+				</li>
+				<?php
+				if(isset($_SESSION['utili'])){
+					echo '<li> <a href="deconnexion.php">Deconnexion </a> </li>';
+					echo '<li> <a href"profil.php"> Profil </a> </li>';
+				}
+				else{
+				echo '<li> <a href="connexion.php"> Connexion </a> </li> ';
+				echo '<li> <a href="FormulaireInscription.php"> Inscription </a> </li>';} ?>
+				
+				<li>
+				<a href="contact/contact.php">Contact</a>
+				</li>
+				
+			</ul>
+		</nav>
+	</header>
   <?php
 $ispseudo=0;
   if($nom!="" && $prenom!="" && $pseudo!="" && $email!="" && $mdp!="" && $ismdp) {
