408bb87a97331f10c4a2581cc34ff482d6d8ba63
Hugo Monchal <hugo.monchal@hotmail.fr>
2022-03-28 13:18:39 +0200

    changement cs profil.php

--- profil.php
+++ profil.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Content-Type"
 content="text/html; charset=UTF-8" />
 
-<link rel="stylesheet" href="styles/styleindexx.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="styles/style.css" type="text/css" media="screen" />
 
 <title>Profil</title>
 
