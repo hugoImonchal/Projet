ca1854a472bb98e2116d3e9ae31e2ee5cbe0ea4e
lamyaeakhtr <lamya.akh@hotmail.fr>
2022-04-06 01:07:00 +0200

    remplissage page contact

--- contact/Lamyae.php
+++ contact/Lamyae.php
@@ -41,9 +41,9 @@
     <br></br>
     Comment me contactez ? 
     <ul>
-        <li>Numéro de téléphone : </li>
-        <li>Mail : </li>
-        <li>Adresse : </li>
+        <li>Numéro de téléphone : 0635141955</li>
+        <li>Mail : lamyae.akhatar@outlook.fr</li>
+        <li>Adresse : 34090 Montpellier </li>
     </ul>
 </body>
 <a href="contact.php">
