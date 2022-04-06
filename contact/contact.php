235d9d17cb0986936e1a7dab2ad8e6cbb66a86ef
lamyaeakhtr <lamya.akh@hotmail.fr>
2022-04-06 20:16:10 +0200

    css

--- contact/contact.php
+++ contact/contact.php
@@ -3,91 +3,82 @@
 <!DOCTYPE html>
 <html>
     <head> 
-        <link rel="stylesheet" href="../styles/style.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="../styles/stylecontact.css" type="text/css" media="screen" />
         <meta http-equiv="Content-Type"content="text/html; charset=UTF-8" />
         <title>Contact</title>
        
     </head>
     <body>
 	
-
-    <header class="main">	
-            <nav>
-                <ul class="menu">
-                <li>
-				<a href="../index.php"> Where2Watch</a>
+	<header class="main">	
+		<nav>
+			<ul class="menu">
+			<li class= "list">
+				<a href="../index.php">Where2Watch</a>
 				</li>
-                    <li>
-                    <a href="../acceuil.php">Accueil</a>
-                    </li>
-                    <?php
-                    if(isset($_SESSION['utili'])){
-                        echo '<li> <a href="../deconnexion.php">Deconnexion </a> </li>';
-                        echo '<li> <a href="../profil.php"> Profil </a> </li>';
-                    }
-                    else{
-                    echo '<li> <a href="../connexion.php"> Connexion </a> </li> ';
-                    echo '<li> <a href="../FormulaireInscription.php"> Inscription </a> </li>';} ?>
-                    
-                    <li>
-                    <a href="contact.php">Contact</a>
-                    </li>
-                    
-                </ul>
-            </nav>
-        </header>
+				<li class= "list">
+				<a href="../acceuil.php">Accueil</a>
+				</li>
+				<?php
+				if(isset($_SESSION['utili'])){
+					echo '<li class= "list"> <a href="../deconnexion.php">Deconnexion </a> </li>';
+					echo '<li class= "list"><a href="../profil.php"> Profil </a> </li>';
+				}
+				else{
+				echo '<li class= "list"> <a href="../connexion.php"> Connexion </a> </li> ';
+				echo '<li class= "list"> <a href="../FormulaireInscription.php"> Inscription </a> </li>';} ?>
+				
+				<li class= "list">
+				<a href="contact.php">Contact</a>
+				</li>
+				
+			</ul>
+		</nav>
+	</header>
 
-        <section class= "fond">
+<section class= "fond">
+	
 
 <br>
 <br>
 <br>
         
 
-        <h1 id="titre">Présentation</h1>
+
+        
+<div class= "container">
+	<div class="information-bar">
+
+		<ul id= "pro">
+			<li class= "active"> Présentation projet</li>
+			<li ><a href="contacteznous.php">Contactez-nous  </a></li>
+
+		</ul>
+
+		<div class="profile">
+			<img src="../images/logo.jpg" alt="image de profil">
+<p class="name">
+    Victoire , Hugo , Lamyae 
+</p>		
+
+</div>
+
+
+	</div>
+
+</div>
+
+
     
-    <p>Nous avons décidé de créer un site web, qui conseil à l’utilisateur la plateforme de
-        vidéo à la demande la plus adaptée à celui-ci, ainsi qu’une sélection de films/séries, en
-        adéquation avec ses goûts. </p>
-    <br>
+    <p>Nous sommes trois étudiants en troisième année de licence MIASHS; Victoire, Hugo et Lamyae.
+        Nous avons crée ce site dans le cadre de notre projet annuel.</p>
+    
     <p>
-        En effet, depuis les années 2000, le streaming commence à voir le jour.
-        Mais qu'est-ce que le streaming ? Les sites de vidéos à la demande permettent simplement
-        de regarder des vidéos ou d'écouter de la musique sur Internet, sans avoir à télécharger de
-        fichier.</p>
-    <br>
-    <p>
-        Cependant, plus récemment, surtout avec la crise sanitaire dans laquelle nous vivons,
-        beaucoup de nouvelles plateformes ont vu le jour et connu un grand essor, comme Netflix,
-        Amazone vidéo, ou même Disney +. </p>
-    <br>
-    <p>
-        Effectivement, la fermeture des cinémas et les confinements ont habitué la population à
-        utiliser de plus en plus les plateformes de streaming. </p>
-    <br>
-    <p>
-        Mais avec l'essor de toutes ces plateformes, laquelle est la plus adaptée à vous ?
-        Car en effet, les sites de vidéos à la demande, malgré le fait qu’ils ont tous un très grand
-        panel de choix de films et séries, il y en aura forcément un qui vous accompagnera mieux
-        dans vos soirées pop cornes ! </p>
-    <br>
-    <p>
-        Notre site a donc pour objectif de trouver à l'utilisateur la plateforme de streaming qui lui
-        correspond le plus et qui répondrait à la majorité de ses critères.
+        Notre site a pour objectif de vous aider à trouver le film ainsi que la plateforme de streaming sur lequel 
+        il se trouve se rapprochant le plus de vos goûts.
     </p>
-    <h1>Qui sommes-nous ?  </h1>
     
-    <p> 4 étudiants de l'université Paul Valery Montpellier 3 :</p>
-    
-    <img src="../images/logo.jpg"/>
-    <br>
-    <ul>
-        <li> <a href="victoire.php"> Victoire Lescroart </a></li>
-        <li><a href="Hugo.php"> Hugo Monchal </a></li>
-        <li><a href="Lamyae.php"> Lamyae Akhatar </a></li>
-        <li><a href="walozz.php"> Walid Zeroual </a></li>
-    </ul>
-</body>
+   
 
     </body>
 </html>
